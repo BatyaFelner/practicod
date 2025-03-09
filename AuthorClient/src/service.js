e70c5129fd293431112dcfee3c3e27ffa6b173ca
BatyaFelner <b0533151235@gmail.com>
2025-03-09 19:53:54 +0200

    emdd"

--- AuthorClient/src/service.js
+++ AuthorClient/src/service.js
@@ -6,12 +6,21 @@ axios.interceptors.response.use(
   response => response, 
   error => {
     console.error("hi"); 
+    console.log("nituv:",process.env.REACT_APP_API_URL); 
+    console.error("nituv:",process.env.REACT_APP_API_URL); 
+    console.log(process.env.REACT_APP_API_URL)
 
     console.error("API Error:", error.response.status,error.response.data); 
     return Promise.reject(error); 
   }
 );
 export default {
+  getTasks: async () => {
+    const result = await axios.get(`${apiUrl}/`)  
+    console.log("API Error:", result.data); 
+  
+    return result.data;
+  },
   getTasks: async () => {
     const result = await axios.get(`${apiUrl}/items`)    
     return result.data;
